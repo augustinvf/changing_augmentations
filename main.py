3383a4030cdff6154262eecdff4084caff331fb3
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-26 15:57:02 +0200

    maj

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import wandb
 wandb.login()
 
+import omegaconf
+
 import torch
 import torch.nn as nn
 
@@ -20,23 +22,27 @@ wandb.init(
     name = "run_1"
 )
 
+# to load the hyperparameters
+
+config = omegaconf.OmegaConf.load("config.yaml")
+
 # data initialization
 
-batch_size = 128
+config.batch_size = 128
 train_dataloader_self_supervised, train_dataloader_supervised, test_dataloader, train_dataset_self_supervised = initialize_dataloader(batch_size)
 
 # tool initialization
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-nb_classes = 10
-input_size_classifier = 512
+nb_classes = config.nb_classes
+input_size_classifier = config.input_size_classifier
 projection_head = SimCLRProjectionHead(512, 512, 128)
 nb_steps = len(train_dataloader_supervised)
 
-nb_cycles = 1
-nb_epochs_self_supervised_by_cycle = 100
-nb_epochs_supervised_by_cycle = 100
+nb_cycles = config.nb_cycles
+nb_epochs_self_supervised_by_cycle = config.nb_epochs_self_supervised_by_cycle
+nb_epochs_supervised_by_cycle = config.nb_epochs_supervised_by_cycle
 
 # hyperparameters for augmentation updates
 
@@ -65,14 +71,17 @@ train_dataset_self_supervised.update_self_supervised_augmentations(self_supervis
 model = Model(projection_head, input_size_classifier, nb_classes).to(device)
 
 criterion_ss = NTXentLoss()
-optimizer_ss = torch.optim.SGD(list(model.backbone.parameters()) + list(model.projection_head.parameters()), 0.3, momentum = 0.9, weight_decay=1e-6)
-scheduler_ss = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_ss, T_max=nb_cycles*nb_epochs_self_supervised_by_cycle, eta_min=0,
-                                                           last_epoch=-1)
+optimizer_ss = torch.optim.SGD(list(model.backbone.parameters()) + list(model.projection_head.parameters()), 
+                               config.optimizer_ss.params.lr, config.optimizer_ss.params.momentum, 
+                               config.optimizer_ss.params.weight_decay)
+scheduler_ss = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_ss, T_max=nb_epochs_self_supervised_by_cycle*nb_cycles,
+                                                          eta_min=config.scheduler_ss.params.eta_min, last_epoch=-1)
 
 criterion_su = nn.CrossEntropyLoss()
-optimizer_su = torch.optim.SGD(model.classifier.parameters(), lr = 0.1, momentum = 0.9, weight_decay=5e-4)
-scheduler_su = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_su, T_max=nb_cycles*nb_epochs_supervised_by_cycle, eta_min=0,
-                                                           last_epoch=-1)
+optimizer_su = torch.optim.SGD(model.classifier.parameters(), config.optimizer_su.params.lr, 
+                               config.optimizer_su.params.momentum, config.optimizer_su.params.weight_decay)
+scheduler_su = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_su, T_max=nb_epochs_supervised_by_cycle*nb_cycles,
+                                                          eta_min=config.scheduler_su.params.eta_min, last_epoch=-1)
 
 # training
 
@@ -89,6 +98,7 @@ for cycle in range (nb_cycles) :
                "accuracy supervised": accuracy/(batch_size*nb_steps),
                "learning rate supervised": scheduler_su.get_last_lr()[0]
                 })
+        print(r_matrix)
     if augmentation_adjustments:
         compute_new_augmentations(nb_classes, power_list, operation_list, old_results, states, r_matrix, threshold, norm)
         update_new_augmentations(self_supervised_augmentations, power_list, operation_list)
