c114063290192c64d8b57fac028bbfb2e5260e10
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-27 18:32:15 +0200

    maj

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ nb_same_time_operations = config.nb_same_time_operations
 power_list = initialize_power_list(nb_classes, nb_augmentations, 5, 5)
 operation_list = initialize_operation_list(nb_classes, nb_augmentations, nb_same_time_operations)   # operations whose powers are currently adjusted
 norm = 2
-threshold = 0.2
+threshold = 0.35
 old_results = torch.tensor([0 for _ in range(nb_classes)])
 states = [True for _ in range(nb_classes)]
 ressemblance_matrix = torch.zeros((nb_classes, nb_classes), device=device)
