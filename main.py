0cac0c521b0361798719049a7e42cf2a15523c09
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-27 10:25:51 +0200

    maj

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ augmentation_adjustments=True
 softmax = nn.Softmax(dim=1)
 nb_augmentations = len_augment_list()
 nb_same_time_operations = config.nb_same_time_operations
-power_list = initialize_power_list(nb_classes, nb_augmentations, 15, 15)
+power_list = initialize_power_list(nb_classes, nb_augmentations, 5, 5)
 operation_list = initialize_operation_list(nb_classes, nb_augmentations, nb_same_time_operations)   # operations whose powers are currently adjusted
 norm = 2
 threshold = 0.2
