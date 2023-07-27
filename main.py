bb2561a8ad90df72ca04622a7829315733375086
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-27 18:41:01 +0200

    maj

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ nb_epochs_supervised_by_cycle = config.nb_epochs_supervised_by_cycle
 
 # hyperparameters for augmentation updates
 
-augmentation_adjustments=False
+augmentation_adjustments=True
 
 softmax = nn.Softmax(dim=1)
 nb_augmentations = len_augment_list()
