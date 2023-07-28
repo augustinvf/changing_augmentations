a50de4e4c93910b8a68a148dfdf85ba76403b293
Augustin <augustin@ama-byrd2.imag.fr>
2023-07-28 10:58:26 +0200

    maj

--- eval.py
+++ eval.py
@@ -1,6 +1,6 @@
 import torch
 
-def test_fct(device, model, test_dataloader):
+def test_fct(device, model, test_dataloader, matrix, nb_experiences_by_class):
     model.eval()
 
     total_tests = 0
@@ -14,8 +14,22 @@ def test_fct(device, model, test_dataloader):
         with torch.no_grad() :
             y_hat = model(image_without_augmentation, "supervised")
 
+        maj_confusion_matrix(matrix, y_hat, labels)
+        nb_experiences_by_class += torch.bincount(labels)
+
         total_tests += labels.shape[0]
         positive_tests += torch.sum(torch.eq(torch.argmax(y_hat, axis = 1), labels))
 
     final_accuracy = positive_tests / total_tests
+
+    matrix = matrix / nb_experiences_by_class.reshape(-1, 1)
+    torch.save(matrix, "./confusion_matrix.pth")
+
     return final_accuracy
+
+def maj_confusion_matrix(matrix, y_hat, labels):
+        for index, label in enumerate(labels) :
+            prediction = torch.argmax(y_hat[index, :]).detach().item()
+            matrix[label,prediction] += 1
+        
+            
